a6a46db5232e739f59f01bb5e45b9cfdfb3659b5
Martha Eichlersmith <martha_eichlersmith@spra.com>
2021-01-19 15:03:30 -0800

    add state only hex map

--- README.md
+++ README.md
@@ -3,17 +3,18 @@
 US Hex Map with just States: [r-graph-gallery.com/hexbin-map](https://www.r-graph-gallery.com/hexbin-map)
 
 
-I've copied the coordinates from the NPR Graphics team: [github.com/nprapps/dailygraphics-templates/blob/master/state_grid_map/index.html](https://github.com/nprapps/dailygraphics-templates/blob/master/state_grid_map/index.html)
+I've copied the coordinates from the NPR Graphics team: [github.com/nprapps/dailygraphics-templates/blob/master/state_grid_map/index.html](https://github.com/nprapps/dailygraphics-templates/blob/master/state_grid_map/index.html). The y values have been reversed so that the plot is not upside down.  
+
+# States  
+This is a direct copy of the NPR state map.  
+![USA Hex Map with States](states/img/usa_s_base.png)
 
 
-# State and Territories 
+# States and Territories 
+This is a direct copy of the NPR state and territory map.  
+![USA Hex Map with States and Territories](states_and_territories/img/usa_st_base.png)
 
-This map is an 'exact' copy of the NPR graphics team's hex map.  (I had to reverse the y values to have the plot not be upside down).  
-![USA Hex Map with States and Territories](state_and_territories/img/usa_st_base.png)
 
-
-# State and Territories Plus
-
-This map adds Palau to the map.  
-
-![USA Hex Map with States and Territories](state_and_territories_plus/img/usa_stp_base.png)
+# States and Territories Plus
+This map adds Palau to the map and changes location of Guam and America Samoa.  
+![USA Hex Map with States and Territories](states_and_territories_plus/img/usa_stp_base.png)
---
+++ states/examples/usa_st_color.R
@@ -0,0 +1,63 @@
+## USA HEX MAP - STATES AND TERRITORIES 
+# COLOR
+
+library(tidyverse)
+
+####################
+#import centers/fort from csv
+centers   <- read_csv("states/hex_file/usa_s_centers.csv")
+spdf_fort <- read_csv("states/hex_file/usa_s_fort.csv")
+
+
+####################
+# estimated population change, 2010-2020
+# data taken from Wikipedia on 2021-01-14
+#source: https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States_by_population
+extra_data <- read_csv("example_data/population_change.csv") %>%
+  #convert character to numeric 
+  mutate(
+      pop_change = str_replace(pop_change, "%", "")
+    , pop_change = str_replace(pop_change, "–", "-")
+    , pop_change = as.numeric(pop_change)/100
+  )
+
+
+####################
+#combine with data sets 
+combined_spdf_fort <- left_join(spdf_fort, extra_data, by = c("id" = "code"))
+combined_centers   <- left_join(centers,   extra_data, by = c("id" = "code"))
+
+
+
+
+#######################
+#plot using ggplot 
+ggplot(combined_spdf_fort) +
+  geom_polygon(aes( x = long, y = lat, group = group, fill = pop_change), color = "grey50") +
+  geom_text(
+      data=combined_centers
+    , aes(x=x, y=y+5, label=id)
+    , fontface = "bold"
+    , size = 4
+  ) +
+  geom_text(
+      data=combined_centers
+    , aes(x=x, y=y-5, label=scales::percent(pop_change, accuracy = 0.1))
+    , size = 3
+  ) +
+  coord_fixed() + #keeps all items square (do not use coord_map(); causes issues)
+  theme_void() +
+  labs(
+    title = "Population Change, 2010 - 2020"
+    , caption = "Source: https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States_by_population as of January 14, 2021"
+  ) + 
+  scale_fill_gradient2(
+      name = "Population\nChange"
+    , low = "red"
+    , mid = "white"
+    , high = "green"
+    , midpoint = 0
+    , labels = scales::percent
+  )
+
+ggsave("states/img/usa_s_color.png", width = 8, height = 5.75, units = c("in"))
