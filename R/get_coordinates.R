9cd0803549a8a2355d40d63afa41e95a6e9ff5e5
Martha Eichlersmith <meichlersmith@csg.org>
2024-05-16 12:22:43 -0500

    use switch instead of if/else statements

--- R/get_coordinates.R
+++ R/get_coordinates.R
@@ -15,6 +15,8 @@
 #'   
 #' @param coords A \code{character} string specifying `"hexmap"` for the 
 #'   hexagon coordinates or `"labels"` for the label coordinates for the hex map
+#'   
+#' @import sf
 #'
 #' @export
 #' 
@@ -23,37 +25,35 @@
 #' @examples
 #' get_coordinates(map = "states50", coords = "hexmap")
 #' get_coordinates(map = "states50", coords = "labels")
-get_coordinates <- function(map, coords = "hexmap"){
+get_coordinates <- function(map = NULL, coords = "hexmap"){
+  
+  
+  stopifnot("Enter 'map' argument" = !is.null(map))
+  stopifnot("Invlaid 'map' argument" = map %in% c("states50", "statesDC", 
+                                                  "statesTerr", "WIOAETA", 
+                                                  "WIOAETAregions"))
+  stopifnot("Invalid 'coords' argument, options are 'hexmap' or 'labels'" = 
+              coords %in% c("hexmap", "labels"))
   
   if (coords == "hexmap"){
-    
-    if (map == "states50"){       usahex::states50 
-    } else if (map == "statesDC"){       usahex::statesDC
-    } else if (map == "statesTerr"){     usahex::statesTerr
-    } else if (map == "WIOAETA"){        usahex::WIOAETA
-    } else if (map == "WIOAETAregions"){ usahex::WIOAETAregions
-    } else { stop("Invalid 'map' argument, current options are:", 
-                  "states50", "statesDC", "statesTerr", "WIOAETA", 
-                  "WIOAETARegions") }
-    
+    switch(map, 
+      "states50"       = usahex::states50, 
+      "statesDC"       = usahex::statesDC, 
+      "statesTerr"     = usahex::statesTerr, 
+      "WIOAETA"        = usahex::WIOAETA, 
+      "WIOAETAregions" = usahex::WIOAETAregions
+    )
   } else if (coords == "labels"){
-    
-           if (map == "states50"){       usahex::states50_labels 
-    } else if (map == "statesDC"){       usahex::statesDC_labels
-    } else if (map == "statesTerr"){     usahex::statesTerr_labels
-    } else if (map == "WIOAETA"){        usahex::WIOAETA_labels
-    } else if (map == "WIOAETAregions"){ usahex::WIOAETAregions_labels
-    } else { stop("Invalid 'map' argument, current options are:", 
-                  "states50", "statesDC", "statesTerr", "WIOAETA", 
-                  "WIOAETARegions") }
-    
+    switch(map, 
+      "states50"       = usahex::states50_labels, 
+      "statesDC"       = usahex::statesDC_labels, 
+      "statesTerr"     = usahex::statesTerr_labels, 
+      "WIOAETA"        = usahex::WIOAETA_labels, 
+      "WIOAETAregions" = usahex::WIOAETAregions_labels
+    ) 
   } else {
-    stop("Invalid 'coords' argument, options are:", 
-         "hexmap or labels")
+    stop("Invalid 'coords' argument, options are: 'hexmap' or 'labels'")
   }
-  
-
-  
   
   
 }
